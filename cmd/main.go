5767d2d114d279f318877b9e45a46358b0341c1d
zondaf12 <ruaridhbell73@googlemail.com>
2024-04-14 21:52:36 +0100

    Added Database connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/ZondaF12/logbook-backend/cmd/api"
+	"github.com/ZondaF12/logbook-backend/config"
+	"github.com/ZondaF12/logbook-backend/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
 	server := api.NewAPIServer(":8080", nil)
 	if err := server.Start(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to database")
 }
