59f3d40109be65d2ce779fbb5875412fca1b31b2
zondaf12 <ruaridhbell73@googlemail.com>
2024-04-15 22:56:12 +0100

    Fixed issue where claims was wrong

--- service/auth/jwt.go
+++ service/auth/jwt.go
@@ -22,7 +22,7 @@ func CreateJWT(secret []byte, userID int) (string, error) {
 	expiration := time.Second * time.Duration(config.Envs.JWTExpirationInSeconds)
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"userID":    strconv.Itoa(userID),
+		"userId":    strconv.Itoa(userID),
 		"expiredAt": time.Now().Add(expiration).Unix(),
 	})
 
